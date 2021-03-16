d5844a6e99be2c303808bdbdcdac7e8ed9c2d6a9
rayel06 <tjdgus7771@gmail.com>
2021-03-16 22:38:28 +0900

    Studying...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Food() {
-  return <h5>I like Potato</h5>
+function Food(props) {
+  return <h5>I like {props.fav}</h5>
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Food fav = "kimchi" />  
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
     </div>
     );
 }
