2a3684922629e976453d745b39395563957f5682
rayel06 <tjdgus7771@gmail.com>
2021-03-18 13:48:31 +0900

    99%

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About'
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-function Food(props) {
-  return <h5>I like {props.fav}</h5>
-}
-
-function App() {
+function App(){
   return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
     );
 }
 
